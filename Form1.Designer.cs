a03fdfbb076656e97e29e725dad0c015cd1ba2dd
lucas-watkins <114666925+lucas-watkins@users.noreply.github.com>
2023-01-04 11:01:56 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace GazetteNotificationsFull
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.browser = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // browser
+            // 
+            this.browser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.browser.Location = new System.Drawing.Point(0, 0);
+            this.browser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.browser.Name = "browser";
+            this.browser.Size = new System.Drawing.Size(1038, 904);
+            this.browser.TabIndex = 0;
+            this.browser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1038, 904);
+            this.Controls.Add(this.browser);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Gazette Notifications";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser browser;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using SHDocVw;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace GazetteNotificationsFull
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {   
+            if (IsConnectedToInternet())
+            {
+                main();
+            }
+            else
+            {
+               while (IsConnectedToInternet() == false)
+                {
+                    Thread.Sleep(3);
+                    if (IsConnectedToInternet()) { main(); }
+                }   
+
+            }
+            
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            var zoom = browser.ActiveXInstance as SHDocVw.InternetExplorer;
+            zoom.ExecWB(SHDocVw.OLECMDID.OLECMDID_OPTICAL_ZOOM, SHDocVw.OLECMDEXECOPT.OLECMDEXECOPT_DODEFAULT, 75, IntPtr.Zero);
+
+            this.Width = browser.Document.Body.ScrollRectangle.Width + 40;//Border
+            this.Height = browser.Document.Body.ScrollRectangle.Height + 40;
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private bool IsConnectedToInternet()
+        {
+            try
+            {
+                Ping ping = new Ping();
+                PingReply reply = ping.Send("8.8.8.8", 3000);
+
+                if (reply.Status == IPStatus.Success) { return true; }
+                else { return false; }
+                
+            }
+            catch { return false; }
+            
+        }
+        private void main()
+        {
+            InitializeComponent();
+            browser.ScriptErrorsSuppressed = true;
+            browser.Navigate(new Uri("https://docs.google.com/spreadsheets/d/e/2PACX-1vTib8OyUFKby5-vRkJHG3l5Ai9r0sC_NejOHHcnOQ498Tqx_q4BzqggD3L01IqZV41VYEg5zDAuNU8N/pubhtml?gid=1416997619&single=true&range=A8:J17&widget=false&headers=false&chrome=false"));
+        }
+
+    }
+
+
+}
+
